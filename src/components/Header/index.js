661cdeb806d729102e2d7deaec55dd29c27a3d04
alexandermcmillan <your-email@domain.com>
2025-02-13 22:48:17 -0800

    logo added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import logo from "../../assets/logo/long-logo.png";
 import "./styles.css";
 
 const Header = () => {
-  return <div className="navbar">Header</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" className="navbar-logo" />
+      <div className="search-bar"> </div>
+      <div className="profile-container"></div>
+    </div>
+  );
 };
 
 export default Header;
