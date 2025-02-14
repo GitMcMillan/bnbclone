bf6a70b0bb8e48af8e6dd499b8c9b29c50ad2bd6
alexandermcmillan <your-email@domain.com>
2025-02-14 15:13:08 -0800

    airbnb home and globe up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../assets/logo/long-logo.png";
 import "./styles.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
 
 const Header = () => {
   return (
@@ -15,7 +16,13 @@ const Header = () => {
           <SearchRoundedIcon className="search-icon" />
         </div>
       </div>
-      <div className="profile-container"></div>
+      <div className="profile-container">
+        <div className="airbnb-your-home">Airbnb your home</div>
+        <div className="globe-div">
+          <LanguageRoundedIcon sx={{ fontSize: "1.3rem" }} />
+        </div>
+        <div className="profile-div"></div>
+      </div>
     </div>
   );
 };
