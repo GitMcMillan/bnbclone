73a9b4dd0794b2d2cd6e8a9e79f57239197a447c
alexandermcmillan <your-email@domain.com>
2025-02-14 16:28:23 -0800

    icon selectioons up. Header finished

--- src/components/Header/ProfileMenu.js
+++ src/components/Header/ProfileMenu.js
@@ -37,11 +37,36 @@ export default function BasicMenu() {
         MenuListProps={{
           "aria-labelledby": "basic-button",
         }}
-        sx={{ borderRadius: "2rem" }}
+        sx={{
+          ".MuiPaper-root": {
+            minWidth: "200px",
+            borderRadius: "0.5rem",
+            boxShadow: "0 1px 2px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 5%)",
+          },
+        }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem className="menu-items" onClick={handleClose}>
+          Signup
+        </MenuItem>
+        <MenuItem className="menu-items" onClick={handleClose}>
+          Login
+        </MenuItem>
+        <div
+          style={{
+            height: "1px",
+            backgroundColor: "var(--grey)",
+            width: "100%",
+          }}
+        />
+        <MenuItem className="menu-items" onClick={handleClose}>
+          Airbnb Your Home
+        </MenuItem>
+        <MenuItem className="menu-items" onClick={handleClose}>
+          Host an experience
+        </MenuItem>
+        <MenuItem className="menu-items" onClick={handleClose}>
+          Help
+        </MenuItem>
       </Menu>
     </div>
   );
